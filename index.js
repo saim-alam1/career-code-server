7a4996c5da3c6e99655472e1d41424a8a8e85f4f
saim-alam1 <allbehassansaim@gmail.com>
2025-07-22 22:09:57 +0600

    Initialized Backend Server

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require("express");
-const cors = require("cors ");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Career Code Cooking");
+});
+
+app.listen(port, () => {
+  console.log(`Career Code Server Is running On port : ${port}`);
+});
